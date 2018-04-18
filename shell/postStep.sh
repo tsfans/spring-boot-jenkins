43aaf4b9ebf01a16fc90a08ad287e6a581b6efd7
HULIN <hu382018@outlook.com>
2018-04-18 17:07:57 +0800

    package as war file

---
+++ shell/postStep.sh
@@ -0,0 +1,25 @@
+DATE=$(date +%Y%m%d)
+export JAVA_HOME PATH CLASSPATH
+JAVA_HOME=/usr/java/jdk1.8.0_162
+PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH
+CLASSPATH=.:$JAVA_HOME/lib:$JAVA_HOME/jre/lib:$CLASSPATH
+SOURCE=/root/.jenkins/workspace/spring-boot-jenkins/target
+DIR=/data/wwwroot/spring-boot-jenkins
+WARFILE=spring-boot-jenkins-0.0.1-SNAPSHOT.war
+
+if [ ! -d $DIR/backup ];then
+   mkdir -p $DIR/backup
+fi
+cd $DIR
+echo "backup war file..."
+if [ -f $WARFILE ];then
+   mv $WARFILE backup/$WARFILE$DATE
+fi
+echo "stop tomcat..."
+systemctl stop tomcat
+echo "copy war file..."
+cp $SOURCE/$WARFILE ./
+echo "start tomcat..."
+systemctl start tomcat
+cd backup
+ls -lt|awk 'NR>5{print $NF}'|xargs rm -rf
--- src/main/java/cn/hl/SpringBootJenkinsApplication.java
+++ src/main/java/cn/hl/SpringBootJenkinsApplication.java
@@ -2,11 +2,26 @@ package cn.hl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+//@SpringBootApplication
+//public class SpringBootJenkinsApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(SpringBootJenkinsApplication.class, args);
+//	}
+//}
 @SpringBootApplication
-public class SpringBootJenkinsApplication {
+public class SpringBootJenkinsApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootJenkinsApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootJenkinsApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootJenkinsApplication.class, args);
+    }
+
 }
